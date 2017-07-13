36d22cc89d73b13f168680d02fdc054ed820abf1
James McCormac <jmccormac001@gmail.com>
2017-07-13 23:31:06 +0100

    using loops like a real programmer :)

--- preview.py
+++ preview.py
@@ -18,34 +18,22 @@ if __name__ == "__main__":
         img_num = 0
         while True:
             imgs = g.glob('*.fits')
+            HFD = {}
             if len(imgs) > 0:
                 img_id = imgs[-1].split('_')[0]
                 if img_id != old_img_id:
                     time.sleep(4)
                     print('Displaying {}*'.format(img_id))
-                    # UT1
-                    ds9Set('GOTO', 'frame 1')
-                    ut1_img_id = '{}_UT1.fits'.format(img_id)
-                    ds9Display('GOTO', ut1_img_id)
-                    if img_num == 0:
-                        ds9Set('GOTO', 'zoom to fit')
-                    ut1_hfd, ut1_std = measureHfd(ut1_img_id)
-                    print('UT1: {} {}'.format(ut1_hfd, ut1_std))
-                    # UT2
-                    ds9Set('GOTO', 'frame 2')
-                    ds9Display('GOTO', '{}_UT2.fits'.format(img_id))
-                    if img_num == 0:
-                        ds9Set('GOTO', 'zoom to fit')
-                    # UT3
-                    #ds9Set('GOTO', 'frame 3')
-                    #ds9Display('GOTO', '{}_UT3.fits'.format(img_id))
-                    #if img_num == 0:
-                    #   ds9Set('GOTO', 'zoom to fit')
-                    # UT4
-                    ds9Set('GOTO', 'frame 4')
-                    ds9Display('GOTO', '{}_UT4.fits'.format(img_id))
-                    if img_num == 0:
-                        ds9Set('GOTO', 'zoom to fit')
+                    # loop over UTs
+                    for i in range(1, 5):
+                        # skip UT3 for now, broken
+                        if i != 3:
+                            ds9Set('GOTO', 'frame {}'.format(i))
+                            ut_img_id = '{}_UT{}.fits'.format(img_id, i)
+                            ds9Display('GOTO', ut_img_id)
+                            HFD[i] = [measureHfd(ut_img_id)]
+                            if img_num == 0:
+                                ds9Set('GOTO', 'zoom to fit')
                     old_img_id = img_id
                     img_num += 1
             time.sleep(1)
